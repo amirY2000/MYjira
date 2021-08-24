9d13041bbb55305d7db2bde91721397ff05d983d
@mhyasaei <mhyasaei@uwaterloo.ca>
2021-08-24 13:04:08 -0400

    Labels are added to the layout

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from layout import MYjira
 def main():
     app = Tk()
     app.title("MYjira")
-    #app.iconbitmap("C:\Users\amira\OneDrive\Desktop\MYjira\icon.png")
     e = MYjira(app)
     app.mainloop()
 if __name__ == "__main__":
