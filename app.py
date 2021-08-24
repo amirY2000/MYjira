fbe079626ae02d9fd5e835e8300f8d55f82661e2
Amir Yasaei <44714388+amirY2000@users.noreply.github.com>
2021-08-24 12:21:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from layout import MYjira
 def main():
     app = tk.Tk()
     app.title("MYjira")
-    #app.iconbitmap("C:\Users\amira\OneDrive\Desktop\MYjira\icon.png")
     e = MYjira(app)
     app.mainloop()
 if __name__ == "__main__":
