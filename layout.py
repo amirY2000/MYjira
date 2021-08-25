651f5e6aa7c82563c63d27db3483f9aa29bf5702
@mhyasaei <mhyasaei@uwaterloo.ca>
2021-08-25 10:57:57 -0400

    task frames are built as objects

--- layout.py
+++ layout.py
@@ -1,30 +1,18 @@
+#Overall layout
 from tkinter import *
+from taskFrames import *
 
-from wx.core import Left
 class MYjira():
     def __init__(self, root):
         background= Frame(root, bg="#38BEA6")#green background
         background.place(relheight=1, relwidth=1)
+
         frame = Frame(background, bg="white")#white sheet where we work on
         frame.place(relheight=0.8,relwidth=0.8, relx=0.1, rely=0.1)
 
-        ToDo_frame = Frame(frame, bg="#F4AFAF")
-        ToDo_frame.place(relheight=0.8,relwidth=0.2, relx=0.1, rely=0.1)
-        ToDo_label = Label(ToDo_frame, text="To Do",bg="#F4AFAF")
-        ToDo_label.config(font=(44))
-        ToDo_label.pack()
-
-        InProgress_frame = Frame(frame, bg="#AFF4EC")
-        InProgress_frame.place(relheight=0.8,relwidth=0.2, relx=0.4, rely=0.1)
-        InProgress_label = Label(InProgress_frame, text="In Progress",bg="#AFF4EC")
-        InProgress_label.config(font=(44))
-        InProgress_label.pack()
-
-        done_frame = Frame(frame, bg="#AFF4B4")
-        done_frame.place(relheight=0.8,relwidth=0.2, relx=0.7, rely=0.1)
-        done_label = Label(done_frame, text="Done", bg="#AFF4B4")
-        done_label.config(font=(44))
-        done_label.pack()
+        toDo_frame = toDo(frame)
+        inProgress_frame = inProgress(frame)
+        done_frame = done(frame)
 
         newTask = Button(frame, text="New Task", padx=20, pady=10)
         newTask.place(x=248, y=775)
---
+++ taskFrames.py
@@ -0,0 +1,27 @@
+#toDo_frame, inProgress_frame, done_frame
+from tkinter import *
+
+class toDo():
+    def __init__(self, frame):
+        ToDo_frame = Frame(frame, bg="#F4AFAF")
+        ToDo_frame.place(relheight=0.8,relwidth=0.2, relx=0.1, rely=0.1)
+        ToDo_label = Label(ToDo_frame, text="To Do",bg="#F4AFAF")
+        ToDo_label.config(font=(44))
+        ToDo_label.pack()
+
+class inProgress():
+    def __init__(self, frame):
+        InProgress_frame = Frame(frame, bg="#AFF4EC")
+        InProgress_frame.place(relheight=0.8,relwidth=0.2, relx=0.4, rely=0.1)
+        InProgress_label = Label(InProgress_frame, text="In Progress",bg="#AFF4EC")
+        InProgress_label.config(font=(44))
+        InProgress_label.pack()
+        
+
+class done():
+    def __init__(self, frame):
+        done_frame = Frame(frame, bg="#AFF4B4")
+        done_frame.place(relheight=0.8,relwidth=0.2, relx=0.7, rely=0.1)
+        done_label = Label(done_frame, text="Done", bg="#AFF4B4")
+        done_label.config(font=(44))
+        done_label.pack()
